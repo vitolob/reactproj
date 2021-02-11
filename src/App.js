2e321fda2cc37578008ba9727530f301518e70ce
Victorino Machava <vitomachava@gmail.com>
2021-02-11 19:11:51 +0200

    Remove unneeded variable on react-hot-loader import line

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Hot, { hot } from "react-hot-loader";
+import { hot } from "react-hot-loader";
 
 function App() {
     return <h1>Basic React Starter Project.</h1>;
