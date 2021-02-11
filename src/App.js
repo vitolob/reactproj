03faf9e189ee4d7227f8c2d1d28095eeda87d454
Victorino Machava <vitomachava@gmail.com>
2021-02-11 19:08:42 +0200

    React Starter Project

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Hot, { hot } from "react-hot-loader";
+
+function App() {
+    return <h1>Basic React Starter Project.</h1>;
+}
+
+export default hot(module)(App);
